79cc9342112ef23cf0bd24511c9851c54b0b12db
zaccodes <bboysanie@gmail.com>
2018-11-14 00:09:21 +0200

    initial commit

---
+++ precache-manifest.92226da23e5f1ff05d0f1cad9b1ad86f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8afbe9d2a63398740b50be6941cdbfe2",
+    "url": "/static/media/project-weather.8afbe9d2.png"
+  },
+  {
+    "revision": "a86fa2d082aafb1c69ec95d15c18022f",
+    "url": "/static/media/project-giphy.a86fa2d0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "43e80db4ce0bb5eef871d1df7702ff54",
+    "url": "/static/media/hangman.43e80db4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "71b0528bdc819d190f07",
+    "url": "/static/js/main.71b0528b.chunk.js"
+  },
+  {
+    "revision": "78755643f6abc46727ae",
+    "url": "/static/js/1.78755643.chunk.js"
+  },
+  {
+    "revision": "71b0528bdc819d190f07",
+    "url": "/static/css/main.29d86cbc.chunk.css"
+  },
+  {
+    "revision": "5e24e8141f22dcc766801390bac71596",
+    "url": "/index.html"
+  }
+];
